32143324c8e3431f874296009dfb90e70f546f53
sangam5756 <sangammunde3@gmail.com>
2024-08-23 18:40:53 +0530

    copyoptionadded

--- src/components/ProblemDetails.jsx
+++ src/components/ProblemDetails.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 
 const ProblemDetails = ({ problem }) => {
-  
+
+  const copy = () => {
+    navigator.clipboard.writeText(problem.solution);
+    alert("Solution is copy to clipboard");
+
+  }
 
   return (
     <div className="p-4 border rounded-lg shadow-lg">
       {/* title */}
-      
+
       <h1 className="text-2xl font-bold mb-2">{problem.id}.{problem.title}</h1>
       <p className="text-gray-700 mb-2"><strong>Difficulty:</strong> {problem.difficulty}</p>
       <p className="text-gray-600 mb-4">{problem.problem_description}</p>
-      
+
       {/* solution  */}
       <div className="mb-4">
-        <h2 className="text-xl font-semibold mb-1">Solution:</h2>
+       <div className='flex justify-between mx-2'>
+       <h2 className="text-xl font-semibold mb-1">Solution:</h2>
+       <p onClick={copy} className='bg-slate-200 cursor-pointer px-2 mb-1 rounded-md'>copy</p>
+       </div>
         <pre className="bg-black  text-green-600 p-2 rounded-md overflow-x-auto">
           <code>{problem.solution}</code>
         </pre>
@@ -23,7 +31,7 @@ const ProblemDetails = ({ problem }) => {
       <p className="text-gray-700 mb-2"><strong>Time Complexity:</strong> {problem.time_complexity}</p>
       <p className="text-gray-700 mb-4"><strong>Space Complexity:</strong> {problem.space_complexity}</p>
 
-    {/* test cases */}
+      {/* test cases */}
 
       <div>
         <h2 className="text-xl font-semibold mb-1">Test Cases:</h2>
@@ -36,7 +44,7 @@ const ProblemDetails = ({ problem }) => {
         ))}
       </div>
 
-        <h2 className='text-xl font-semibold mb-1'>Brief Explaination</h2>
+      <h2 className='text-xl font-semibold mb-1'>Brief Explaination</h2>
       <div>
         <p>{problem.brief_explanation}</p>
       </div>
