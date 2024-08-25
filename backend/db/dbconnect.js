c3dfa1ce7ebbfe0f4f7e4c885f81984f76345985
sangam5756 <sangammunde3@gmail.com>
2024-08-25 13:21:29 +0530

    issues

--- backend/db/dbconnect.js
+++ backend/db/dbconnect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const dbcon = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(" mongodb+srv://sangammunde3:69ZEogw3grwMXHyk@cluster0.7e0zod4.mongodb.net/smcode");
 
   } catch (error) {
     console.log(error);
