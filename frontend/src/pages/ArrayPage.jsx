0f239bbebcbccf1213055613a608b085de96931f
sangam5756 <sangammunde3@gmail.com>
2024-08-25 13:46:15 +0530

    backend-connection-done

--- frontend/src/pages/ArrayPage.jsx
+++ frontend/src/pages/ArrayPage.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-import problemData from "../resources/Data/ArrayData"
+import React, { useEffect, useState } from 'react'
+// import problemData from "../resources/Data/ArrayData"
 import ProblemDetails from '../components/ProblemDetails'
 
 const ArrayPage = () => {
-  console.log("consoolle in problemdata", problemData)
+  const [problemData, setProblemData] = useState([])
+  const problemfetch = async () => {
+    const response = await fetch("https://smcode-production.up.railway.app/api/questions");
+    const data = await response.json();
+    setProblemData(data)
+    console.log(data);
+  }
+
+  useEffect(() => {
+          
+    problemfetch()
+  }, []);
+
   return (
     <>
       <div className=''>
