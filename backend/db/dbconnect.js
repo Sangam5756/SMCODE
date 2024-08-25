22f303197f3f53de40146703e4a066c9a706b0f1
sangam5756 <sangammunde3@gmail.com>
2024-08-25 13:23:33 +0530

    urlsmongodb

--- backend/db/dbconnect.js
+++ backend/db/dbconnect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const dbcon = async () => {
   try {
-    await mongoose.connect(" mongodb+srv://sangammunde3:69ZEogw3grwMXHyk@cluster0.7e0zod4.mongodb.net/smcode");
+    await mongoose.connect("mongodb+srv://sangammunde3:69ZEogw3grwMXHyk@cluster0.7e0zod4.mongodb.net/smcode");
 
   } catch (error) {
     console.log(error);
