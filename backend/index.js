8bb974859f5b21240987c2e2632d4e1952a97e3d
sangam5756 <sangammunde3@gmail.com>
2024-08-25 13:16:10 +0530

    cors=issue

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,11 @@ import express from "express";
 import { configDotenv } from "dotenv";
 import { dbcon } from "./db/dbconnect.js";
 import questionRouter from "./routes/Question.router.js"
+import cors from "cors"
 
 configDotenv();
 const app = express();
-
+app.use(cors())
 const PORT = process.env.PORT;
 
 app.use(express.json());
