e6eda051e5a027e6ce301e0d95c987121e5516a9
sangam5756 <sangammunde3@gmail.com>
2024-08-27 08:04:37 +0530

    loader-added

--- frontend/src/pages/DisplayPage.jsx
+++ frontend/src/pages/DisplayPage.jsx
@@ -2,20 +2,24 @@ import React, { useEffect, useState } from 'react'
 import axios from "axios"
 import { useLocation, useSearchParams } from 'react-router-dom';
 import ProblemDetails from '../components/ProblemDetails'
+import Loader from '../components/Loader';
 
 const DisplayPage = () => {
   const [problemData, setProblemData] = useState([])
   const [error, setError] = useState(false);
+  const[loading,setLoading] = useState(true);
 
   const location = useLocation();
   const category = location.pathname.slice(1);
 
 
   const handlesearch = async () => {
+    setLoading(true);
     // const response = await axios.post("http://localhost:5000/api/category", { category: category })
     const response = await axios.post("https://smcodebackend5756.onrender.com/api/category", { category: category })
 
     if (response.data.success) {
+      setLoading(false)
       setProblemData(response.data.data)
       setError(false);
 
@@ -23,6 +27,7 @@ const DisplayPage = () => {
 
     if (response.data.error) {
       setError(true);
+      setLoading(false);
       setProblemData(null)
     }
 
@@ -42,6 +47,9 @@ const DisplayPage = () => {
 
   return (
     <>
+    {loading && <Loader/>}
+
+    
       {error && <p className='mt-2 text-center bg-slate-500  text-xl'>Topic Not Found it will be  available soon</p>}
       <div className=''>
         {
